7cbf8e5327cf21f79594129e204c5adb900b1ad8
Lucas_Bernardes <43051954+wunbo21@users.noreply.github.com>
2021-04-06 19:26:28 -0300

    Create stack.js

---
+++ Stack/src/stack.js
@@ -0,0 +1,63 @@
+function Stack() {
+  // Atributos Privados
+  let count = 0
+  let itens = {}
+
+  // Métodos Públicos
+  this.push = elemento => {
+    itens[count] = elemento
+    console.log(`Elemento ${elemento}, adicionado a pilha`)
+    count++
+  }
+
+  this.size = () => count
+
+  this.isEmpty = () => count === 0 
+
+  this.pop = () => {
+    if(this.isEmpty())
+      return undefined
+    count--
+    let topo = itens[count]
+    delete itens[count]
+    return topo
+  }
+
+  this.peek = () => {
+    if(this.isEmpty())
+      return undefined
+    return itens[count - 1]
+  }
+
+  this.clear = () => {
+    count = 0
+    itens = {}
+    console.log(`\nPilha foi limpa(Método 1).\nPilha vazia? ${this.isEmpty()}\n`)
+  }
+
+  this.clear_2 = () => {
+    while(!this.isEmpty())
+      this.pop()
+    console.log(`\nPilha foi limpa(Método 2).\nPilha vazia? ${this.isEmpty()}\n`)
+  }
+
+  this.toString = () => {
+    if(this.isEmpty())
+      return 'Pilha Vazia'
+    let objString = `${itens[0]}`
+    for(let i = 1; i < count; i++){
+      objString = `${objString}, ${itens[i]}`
+    }
+    return objString
+  }
+
+  this.toStringReverse = () => {
+    if(this.isEmpty())
+      return 'Pilha Vazia'
+    let objString = `${itens[0]}`
+    for(let i = 1; i < count; i++){
+      objString = `${itens[i]}, ${objString}`
+    }
+    return objString
+  }
+}
