45cabbff70ea1a1c8e2a6bf848377c73f1eca83e
Lucas_Bernardes <43051954+wunbo21@users.noreply.github.com>
2021-04-10 18:07:35 -0300

    Create queue.js

---
+++ Queue/src/queue.js
@@ -0,0 +1,51 @@
+function Queue(){
+
+  // Atributos Privados
+  let count = 0
+  let first = 0
+  let itens = {}
+
+  // Métodos Públicos
+  this.enqueue = element => {
+    itens[count] = element
+    count++
+  }
+
+  this.dequeue = () => {
+    if(this.isEmpty())
+      return undefined
+    const result = itens[first]
+    delete itens[first]
+    first++
+    return result
+  }
+
+  this.isEmpty = () => this.size() === 0
+
+  this.size = () => count - first
+
+  this.front = () => {
+    if(this.isEmpty())
+      return undefined
+    return itens[first]
+  }
+
+  this.clear = () => {
+    while(!this.isEmpty())
+      this.dequeue()
+    console.log(`A fila terminou de ser limpada.`)
+  }
+
+  this.toString =() => {
+    if(this.isEmpty())
+      return ''
+    let objString = `${itens[first]}`
+    for(let i = first + 1; i < count; i++)
+      objString = `${objString}, ${itens[i]}`
+    return objString
+  }
+}
+
+module.exports = {
+  Queue
+}
